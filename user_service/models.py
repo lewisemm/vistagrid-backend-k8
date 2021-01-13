4336e3fde57271b04c0bed7c5a39cee9837f305a
Lewis Kabui <lewisemm@users.noreply.github.com>
2021-01-13 21:12:05 +0300

    Change table name from user to users
    - Postgresql has a default table with the name "user"
    - Change the user model to use "users" table to avoid ambiguity

--- user_service/models.py
+++ user_service/models.py
@@ -14,6 +14,7 @@ class User(db.Model):
     """
     User model.
     """
+    __tablename__ = "users"
     user_id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
