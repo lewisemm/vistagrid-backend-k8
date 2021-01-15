88a26ba7bbbee3624b43bc0a7d101dffba7beaad
Lewis Kabui <lewisemm@users.noreply.github.com>
2021-01-15 11:20:52 +0300

    Update config values and cleanup code

--- user_service/config/config.py
+++ user_service/config/config.py
@@ -7,6 +7,4 @@ class CommonConfig(object):
 
 class DevConfig(CommonConfig):
     SQLALCHEMY_DATABASE_URI = os.environ['SQLALCHEMY_DATABASE_URI']
-    # SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
     JWT_SECRET_KEY = os.environ['JWT_SECRET_KEY']
-    # JWT_SECRET_KEY = 'super secret'
