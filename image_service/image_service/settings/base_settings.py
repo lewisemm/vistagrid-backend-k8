17c44b694e0fccfc490d229ae2e24f5e202f5010
Lewis Kabui <lewisemm@users.noreply.github.com>
2022-11-03 01:30:14 +0300

    Use TemporaryFileUploadHandler only
    - Default config uses TemporaryFileUploadHandler (when file > 2.5MB)
    and InMemoryUploadedFile (when file size < 2.5MB)
    - Use TemporaryFileUploadHandler only. It is preferred because it has
    temporary_file_path method that can be used to locate file on disk.

--- image_service/image_service/settings/base_settings.py
+++ image_service/image_service/settings/base_settings.py
@@ -102,3 +102,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+FILE_UPLOAD_HANDLERS = [
+    'django.core.files.uploadhandler.TemporaryFileUploadHandler',
+]
