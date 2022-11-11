ab6e1b058e6cdd3ae3cecbb4058f37e1dad7ab14
Lewis Kabui <lewisemm@users.noreply.github.com>
2022-11-11 23:12:03 +0300

    Use more explicit name s3_resource
    - This has been done to eliminate confusion with s3_client that will
    be used to create presigned urls

--- image_service/aws/s3.py
+++ image_service/aws/s3.py
@@ -5,9 +5,9 @@ import boto3
 
 class S3Operations:
     def __init__(self):
-        self.s3 = boto3.resource('s3')
+        self.s3_resource = boto3.resource('s3')
         bucket_name = os.environ['S3_BUCKET']
-        self.bucket = self.s3.Bucket(bucket_name)
+        self.bucket = self.s3_resource.Bucket(bucket_name)
 
     def upload_photo(self, binary_data, filename):
         self.bucket.put_object(Key=filename, Body=binary_data)
@@ -20,7 +20,7 @@ class S3Operations:
 
     def delete_bucket_object(self, object_key):
         bucket_name = os.environ['S3_BUCKET']
-        s3_object = self.s3.Object(bucket_name, object_key)
+        s3_object = self.s3_resource.Object(bucket_name, object_key)
         s3_object.delete()
 
     def __repr__(self):
