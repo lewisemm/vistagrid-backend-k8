508dbe6d212b414a2f392bab685e706d0fbf4c47
Lewis M. Kabui <lewisemm@users.noreply.github.com>
2023-07-31 23:56:38 +0300

    Add token expiry configuration

--- user_service/config/config.py
+++ user_service/config/config.py
@@ -1,10 +1,15 @@
 import os
+import datetime
 
 
 class CommonConfig(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
+
 class DevConfig(CommonConfig):
     SQLALCHEMY_DATABASE_URI = os.environ['SQLALCHEMY_DATABASE_URI']
     JWT_SECRET_KEY = os.environ['JWT_SECRET_KEY']
+    JWT_ACCESS_TOKEN_EXPIRES = datetime.timedelta(
+        minutes=int(os.environ['JWT_ACCESS_TOKEN_EXPIRES_MINUTES'])
+    )
