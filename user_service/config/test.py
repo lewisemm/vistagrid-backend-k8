bb617e2dd2e2ab3b9bb207ada18c970d18898e0d
Lewis M. Kabui <lewisemm@users.noreply.github.com>
2023-10-14 22:09:10 +0300

    Fix typo

--- user_service/config/test.py
+++ user_service/config/test.py
@@ -3,7 +3,7 @@ import os
 from user_service.config.base import CommonConfig
 
 class TestConfig(CommonConfig):
-    TESING = True
+    TESTING = True
     JWT_SECRET_KEY = os.environ['JWT_SECRET_KEY']
     SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
     SERVER_NAME = os.environ['SERVER_NAME']
