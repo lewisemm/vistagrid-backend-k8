23aaf7148054f0d3fb03d50bab1a6aff4ff59780
Lewis M. Kabui <lewisemm@users.noreply.github.com>
2023-12-05 23:51:26 +0300

    Add and test rate limiter on refresh route

--- user_service/tests/test_rate_limits.py
+++ user_service/tests/test_rate_limits.py
@@ -7,6 +7,7 @@ from user_service.tests.fixtures.common import (
     client,
     credentials,
     existing_user,
+    redis_mock
 )
 
 def test_login_route_rate_limit(client, existing_user, credentials):
@@ -29,6 +30,40 @@ def test_login_route_rate_limit(client, existing_user, credentials):
         res = client.post(
             url,
             data=json.dumps(credentials),
+            content_type='application/json'
+        )
+        assert res.status_code == 429
+
+def test_refresh_route_rate_limit(client, existing_user, credentials, redis_mock):
+    """
+    Tests that successful requests to `/refresh` route are limited to two per 15
+    minute window.
+    """
+    with client.application.app_context():
+        url = url_for('user-auth')
+        # obtain refresh token
+        res = client.post(
+            url,
+            data=json.dumps(credentials),
+            content_type='application/json'
+        )
+        assert res.status_code == 200
+        assert 'refresh_token' in res.json
+        refresh_token = res.json['refresh_token']
+        # accepts only two requests to refresh within a 15 minute window.
+        url = url_for('refresh')
+        for _ in range(2):
+            res = client.post(
+                url,
+                headers={'Authorization': f'Bearer {refresh_token}'},
+                content_type='application/json'
+            )
+            assert res.status_code == 200
+            assert 'access_token' in res.json
+        # third request is denied
+        res = client.post(
+            url,
+            headers={'Authorization': f'Bearer {refresh_token}'},
             content_type='application/json'
         )
         assert res.status_code == 429
